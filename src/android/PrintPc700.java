68d8578de7dec79bf278bb840aff6a8b99af8e71
RidiQirici <ridi.qirici@imb.al>
2015-09-16 13:18:58 +0200

    Thread ne java versioni 1.1

--- src/android/PrintPc700.java
+++ src/android/PrintPc700.java
@@ -54,65 +54,64 @@ public class PrintPc700 extends CordovaPlugin{
 	public static final String PRINT_IMG = "printImg";
 	protected static final String TAG = "Pc700PrintPlugin";
 	private CallbackContext mesazhi;
-    private boolean veprimiKryer;
-    static PrinterClassSerialPort printerClass = null;
-    private Thread autoprint_Thread;
-    private String perPrintim;
-    private boolean printimi = true;
-    private String mesazhPrintim = "";
-    
+	private boolean veprimiKryer;
+	static PrinterClassSerialPort printerClass = null;
+	private Thread autoprint_Thread;
+	private String perPrintim;
+	private boolean printimi = true;
+	private String mesazhPrintim = "";
+
 	@Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		this.mesazhi = callbackContext;
 		this.veprimiKryer = true;
-		
-        if (PRINT_TEXT.equals(action)) {
-        	mesazhPrintim = args.getString(0);
-            if (!mesazhPrintim.isEmpty()) {
-            	
-            	Thread t = new Thread(){
-            	    public void run(){
-            	    	veprimiKryer = printoTekstin(mesazhPrintim);
-            	    }
-            	};
-            	t.start();
-            	
-                if(veprimiKryer)
-                {
-                	this.veprimiKryer = true;
-                	System.out.println(TAG + " Printimi u krye me sukses!");
-                	Log.d(TAG, "Printimi u krye me sukses!");
-                	this.mesazhi.success("Printimi u krye me sukses!");
-                	
-                }
-                else
-                {
-                	this.veprimiKryer = false;
-                	System.out.println(TAG + " Ndodhi nje gabim gjate printimit!");
-                	Log.d(TAG, "Ndodhi nje gabim gjate printimit!");
-                	this.mesazhi.error("Ndodhi nje gabim gjate printimit!");
-                	
-                }
-                	
-            } else {
-                this.veprimiKryer = false;
-                System.out.println(TAG + " Ndodhi nje JSON Exception!");
-                Log.d(TAG, "Ndodhi nje JSON Exception ");
-                this.mesazhi.error("Perdoruesi nuk ka specifikuar te dhena per tu printuar");
-                
-            }
-        } else if (PRINT_IMG.equals(action)) {
-            //TODO
-        } else {
-        	this.veprimiKryer = false;
-        	System.out.println(TAG + " Veprim i pavlefshem : Eshte kaluar veprimi " + action + "!");
-        	Log.d(TAG, "Veprim i pavlefshem : Eshte kaluar veprimi " + action + "!");
-        	this.mesazhi.error("Veprim i pavlefshem!");
-            
-        }
-        return veprimiKryer;
-    }
-        
+
+		if (PRINT_TEXT.equals(action)) {
+			mesazhPrintim = args.getString(0);
+			if (!mesazhPrintim.isEmpty()) {
+
+				new Thread(new Runnable() {
+					public void run() {
+						veprimiKryer = printoTekstin(mesazhPrintim);
+					}
+				}).start();
+
+				if(veprimiKryer)
+				{
+					this.veprimiKryer = true;
+					System.out.println(TAG + " Printimi u krye me sukses!");
+					Log.d(TAG, "Printimi u krye me sukses!");
+					this.mesazhi.success("Printimi u krye me sukses!");
+
+				}
+				else
+				{
+					this.veprimiKryer = false;
+					System.out.println(TAG + " Ndodhi nje gabim gjate printimit!");
+					Log.d(TAG, "Ndodhi nje gabim gjate printimit!");
+					this.mesazhi.error("Ndodhi nje gabim gjate printimit!");
+
+				}
+
+			} else {
+				this.veprimiKryer = false;
+				System.out.println(TAG + " Ndodhi nje JSON Exception!");
+				Log.d(TAG, "Ndodhi nje JSON Exception ");
+				this.mesazhi.error("Perdoruesi nuk ka specifikuar te dhena per tu printuar");
+
+			}
+		} else if (PRINT_IMG.equals(action)) {
+			//TODO
+		} else {
+			this.veprimiKryer = false;
+			System.out.println(TAG + " Veprim i pavlefshem : Eshte kaluar veprimi " + action + "!");
+			Log.d(TAG, "Veprim i pavlefshem : Eshte kaluar veprimi " + action + "!");
+			this.mesazhi.error("Veprim i pavlefshem!");
+
+		}
+		return veprimiKryer;
+	}
+
 	public boolean printoTekstin(String stringaXPrintim) {
 		this.veprimiKryer = true;
 		try {
@@ -130,10 +129,10 @@ public class PrintPc700 extends CordovaPlugin{
 				this.mesazhi.error("Ndodhi nje problem gjate hapjes se portes seriale 38400!");
 				return this.veprimiKryer;
 			}
-			
-			
+
+
 			this.veprimiKryer = printerClass.printText(stringaXPrintim);
-							
+
 			if (!this.veprimiKryer)
 			{
 				printerClass.close();
@@ -149,18 +148,18 @@ public class PrintPc700 extends CordovaPlugin{
 			this.veprimiKryer = false;
 			System.out.println(TAG + " " +  e.getMessage());
 			Log.e(TAG, e.getMessage());
-        	this.mesazhi.error("Gabim gjate printimit te tekstit! " + e.getMessage() + " " + e.toString() + " " + this.veprimiKryer );
+			this.mesazhi.error("Gabim gjate printimit te tekstit! " + e.getMessage() + " " + e.toString() + " " + this.veprimiKryer );
 
 			return this.veprimiKryer;
 		}
-    }
-	
+	}
+
 	public boolean printoTekstinMetoda2(String stringaXPrintim) {
 		this.veprimiKryer = true;
 		try {
 			printerClass = new PrinterClassSerialPort();
 			this.veprimiKryer = printerClass.open();
-			
+
 			if (printerClass.setSerialPortBaudrate(38400))
 			{
 				this.veprimiKryer = printerClass.printText(stringaXPrintim);
@@ -178,10 +177,10 @@ public class PrintPc700 extends CordovaPlugin{
 			return this.veprimiKryer;
 		} catch (Exception e) {
 			this.veprimiKryer = false;
-        	this.mesazhi.error("Gabim gjate printimit te tekstit! " + e.getMessage() + " " + e.toString() + " " + this.veprimiKryer );
+			this.mesazhi.error("Gabim gjate printimit te tekstit! " + e.getMessage() + " " + e.toString() + " " + this.veprimiKryer );
 			Log.e(TAG, e.getMessage());
 			return this.veprimiKryer;
 		}
-    }
+	}
 }
 
